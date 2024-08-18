743c86c8e61cb91279815661bb75bc4fd1487c82
James Wander <james.souza@fatec.sp.gov.br>
2024-08-18 17:44:56 -0300

    Added rooms shop menu

--- Assets/Scripts/PlayerHUD.cs
+++ Assets/Scripts/PlayerHUD.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -5,6 +6,7 @@ public class PlayerHUD : MonoBehaviour
 {
     [SerializeField] private UIDocument doc;
     [SerializeField] private StyleSheet css;
+    [SerializeField] private string[] rooms = new string[3];
 
     private void Start()
     {
@@ -14,9 +16,26 @@ public class PlayerHUD : MonoBehaviour
         var roomsShop = new VisualElement();
         roomsShop.AddToClassList("rooms-shop");
 
-        root.Add(roomsShop);
+        var roomsShopTitle = new Label();
+        roomsShopTitle.text = "Rooms Shop";
+        roomsShopTitle.AddToClassList("rooms-shop-title");
+        roomsShop.Add(roomsShopTitle);
 
+        root.Add(roomsShop);
+        for (int i = 0; i < rooms.Length; i++) { 
+            var room = new Button();
+            room.text = rooms[i];
+            room.AddToClassList("room");
+            Debug.Log(rooms[i]);
+            room.RegisterCallback<MouseUpEvent, string>(selectRoomType, rooms[i]);
+            roomsShop.Add(room);
+        }
     }
 
+    public void selectRoomType(MouseUpEvent evt, string texto)
+    {
+        Debug.Log("Clicou");
+        Debug.Log(texto);
+    }
 
 }

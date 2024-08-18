b6848519d16dbd7739b518bf795632a6451133e6
gabrielligoski <gabriel.ligoski@gmail.com>
2024-08-18 01:50:02 -0300

    added spawner room, added enemies mechanics and added navmesh

--- Assets/Scripts/GenerateMap.cs
+++ Assets/Scripts/GenerateMap.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 
 public class GenerateMap : MonoBehaviour
 {
-    public static List<List<GameObject>> createMap(int size, float roomGap, GameObject coreRoom, GameObject blankRoom)
+    public static (List<List<GameObject>>, GameObject, GameObject) createMap(int size, float roomGap, GameObject coreRoom, GameObject blankRoom, GameObject floor)
     {
+        GameObject core = null;
         var map = new List<List<GameObject>>();
-        var mapParent = new GameObject("Map");
+        var mapParent = new GameObject("Map").transform;
         for (int i = 0; i < size; i++)
         {
             var row = new List<GameObject>();
@@ -17,12 +18,20 @@ public class GenerateMap : MonoBehaviour
                 var z = j + j * roomGap;
 
                 if (i == size / 2 && j == size / 2)
-                    row.Add(Instantiate(coreRoom, new Vector3(x, 0, z), Quaternion.identity, mapParent.transform));
+                {
+                    core = Instantiate(coreRoom, new Vector3(x, 0, z), Quaternion.identity, mapParent);
+                    row.Add(core);
+                }
                 else
-                    row.Add(Instantiate(blankRoom, new Vector3(x, 0, z), Quaternion.identity, mapParent.transform));
+                    row.Add(Instantiate(blankRoom, new Vector3(x, 0, z), Quaternion.identity, mapParent));
             }
         }
 
-        return map;
+        var floorInstance = Instantiate(floor, new Vector3(size / 2, 0, size / 2), Quaternion.identity, mapParent);
+        // offset by half a block
+        floorInstance.transform.position += new Vector3(.5f, -.5f, .5f);
+        // plane is 9x9 by
+        floorInstance.transform.localScale = Vector3.one * (size / 9f);
+        return (map, floorInstance, core);
     }
 }

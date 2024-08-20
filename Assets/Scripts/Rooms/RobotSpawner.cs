21c91ea0585052b71bcdb5ee77d24b4661273f3d
gabrielligoski <gabriel.ligoski@gmail.com>
2024-08-20 03:53:07 -0300

    feat: made characters small and fixed robot spawners aswell added dificulty

--- Assets/Scripts/Rooms/RobotSpawner.cs
+++ Assets/Scripts/Rooms/RobotSpawner.cs
@@ -1,23 +1,32 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RobotSpawner : Room
 {
-    [SerializeField] private Dictionary<string, GameObject> robotPfb;
-
-    public List<GameObject> spawns = new List<GameObject>();
+    public List<GameObject> robotPfb = new List<GameObject>();
 
     public override RoomType roomType()
     {
         return RoomType.robot_spawner;
     }
 
+    private void Start()
+    {
+        GameMaster.Instance.robotSpawners.Add(this);
+        if (!GameMaster.Instance.hasStartedWaves)
+        {
+            StartCoroutine(GameMaster.Instance.SpawnWaves());
+            GameMaster.Instance.hasStartedWaves = true;
+        }
+    }
 
     public void Spawn(Dictionary<string, int> robotsToSpawn)
     {
         foreach (var robot in robotsToSpawn)
             for (int i = 0; i < robot.Value; i++)
-                Instantiate(robotPfb[robot.Key], gameObject.transform.position, Quaternion.identity);
+                Instantiate(robotPfb.Find(rb => rb.name.Equals(robot.Key)), gameObject.transform.position, Quaternion.identity);
 
     }
 
